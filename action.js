6fc5d3d7ff65ccb17c80fe36922ce29463f7155e
Aditya Suryawanshi <avsuryawanshi03@gmail.com>
2024-10-25 12:18:18 +0530

    cHANGE

--- action.js
+++ action.js
@@ -1,8 +1,1 @@
-window.onscroll = function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-};
+
